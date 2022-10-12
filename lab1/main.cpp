6689c84f19d64325c100c1eeff9b1319d2fe7dfc
Fylyp1010 <77103796+Fylyp19@users.noreply.github.com>
2022-10-12 19:07:38 +0200

    Wersja finalna

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,26 +4,26 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <algorithm>
+
 
 int main(int argc, char **argv) {
     using namespace std;
     map<string, function<double(vector<double>)> > func;
-    func["add"] = [](auto arg) { return arg.at(0) + arg.at(1); };
     func["sin"] = [](auto arg) { return sin(arg.at(0)); };
+    func["add"] = [](auto arg) { return arg.at(0) + arg.at(1); };
     func["mod"] = [](auto arg) { return (int) arg.at(0) % (int) arg.at(1); };
     try {
         vector<string> func_args(argv, argv + 2);
-        vector<string> num_args(argv + 2, argv + argc);
+        vector<string> val_args(argv + 2, argv + argc);
         auto selected_f = func_args.at(1);
-        std::vector<double> doubleVector(num_args.size());
-        std::transform(num_args.begin(), num_args.end(), doubleVector.begin(), [](const std::string &val) {
+        std::vector<double> doubleVector(val_args.size());
+        std::transform(val_args.begin(), val_args.end(), doubleVector.begin(), [](const std::string &val) {
             return std::stod(val);
         });
         auto result = func.at(selected_f);
-        cout << "Rozwiązanie = " << result(doubleVector);
+        cout << "Rozwiazanie = " << result(doubleVector);
     } catch (std::out_of_range aor) {
-        cout << "Podaj brakujące argumenty.";
+        cout << "Uzupełnij polecenie.";
     }
 
     return 0;
